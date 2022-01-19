88cf4496f2e273bb7382a763675cd7795a9b6956
zoucw <326359613@qq.com>
2022-01-19 19:50:33 +0800

    配置修改

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,9 +1,18 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: chunwen
+ * @Date: 2021-11-02 10:31:59
+ * @LastEditors:
+ * @LastEditTime: 2022-01-19 19:50:10
+ */
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  publicPath: '/react-template',
   devServer: {
-    
+
   }
 });
