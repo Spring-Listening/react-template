fda32dd47feb454a6990753221ff81b26002d74b
zoucw <326359613@qq.com>
2022-01-20 09:58:21 +0800

    path修改

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -4,7 +4,7 @@
  * @Author: chunwen
  * @Date: 2021-11-02 10:31:59
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-01-19 22:12:05
+ * @LastEditTime: 2022-01-20 09:57:11
  */
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
@@ -12,7 +12,7 @@ const baseConfig = require('./webpack.base.config');
 module.exports = merge(baseConfig, {
   mode: 'production',
   output: {
-    publicPath: '/react-template',
+    // publicPath: '/react-template',
   },
   devServer: {},
 });
